998d15f3dec02e51efba9e72a2f7db82f030be99
sachin <sachin.dubey@craftsvilla.com>
2024-02-05 16:03:04 +0000

    lambda handler added

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,8 +1,11 @@
 import json
-from utils.gephi_restructure import db_connect, db_pull, db_push
+from utils.gephi_restructure import main
+
 
 def lambda_handler(event, context):
     # TODO implement
+
+    main()
     return {
         'statusCode': 200,
         'body': json.dumps('Gephi restructure script. . .')
